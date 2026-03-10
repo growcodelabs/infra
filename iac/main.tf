db31941590f0e8ed90735e56c8a613ec73466b3d
Thiago Lima <thiago.lima@noredink.com>
2026-03-10 09:46:01 -0300

    feat: create a container registry

--- iac/main.tf
+++ iac/main.tf
@@ -1,9 +1,26 @@
+locals {
+  region   = "nyc3"
+  registry = "growcodelabs"
+}
+
+import {
+  id = local.registry
+  to = digitalocean_container_registry.registry
+}
+
+resource "digitalocean_container_registry" "registry" {
+  name   = local.registry
+  region = local.region
+
+  subscription_tier_slug = "starter"
+}
+
 module "environment" {
   source = "./modules/environment"
 
   name     = terraform.workspace
   ip_range = "10.0.0.0/24"
-  region   = "nyc3"
+  region   = local.region
 
   databases = {
     shared = {}
