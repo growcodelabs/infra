cf73cc98b293c3bca35c0a958575a14cc8377e32
Thiago Lima <thiago.lima@noredink.com>
2026-03-10 18:58:55 -0300

    chore: add starship support

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,13 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = with pkgs; [ git opentofu postgresql_18 doctl argocd ];
+  packages = with pkgs; [ git opentofu postgresql_18 doctl argocd starship ];
 
   languages.opentofu = {
     enable     = true;
     lsp.enable = true;
   };
 
-  dotenv.enable = true;
+  dotenv.enable   = true;
+  starship.enable = true;
 }
